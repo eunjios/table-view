cecfd05a6e56da2a11f0bdd1d06f4b6ca6a1214d
Eunji Lee <77034159+eunjios@users.noreply.github.com>
2021-02-04 22:13:31 +0900

    MVVM
    MVVM패턴 연습

--- Table View/DetailViewController.swift
+++ Table View/DetailViewController.swift
@@ -9,19 +9,41 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    // MVVM
+    
+    // Model
+    // - FilmInfo
+    // > FilmInfo 만들자
+    
+    // View
+    // - detailImage
+    // > view 들은 viewModel 을 통해서 구성 되어야 한다..
+    
+    // ViewModel
+    // - FilmViewModel
+    // > 뷰 레이어에서 필요한 메서드 만들기
+    // > 모델 가지고 있기 --> FilmInfo
+    
     @IBOutlet weak var detailImage: UIImageView!
+    @IBOutlet weak var detailDateLabel: UILabel!
     
     var index: Int?
-//    var listTitle: String?
+    var filmInfo: FilmInfo?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         detailImage.layer.zPosition = -1
+        detailDateLabel.layer.zPosition = 2
         
-        if let index = self.index {
+        updateUI()
+    }
+    
+    func updateUI() {
+        if let filmInfo = self.filmInfo, let index = self.index {
             let img = UIImage(named: "\(index).jpg")
             detailImage.image = img
+            detailDateLabel.text = "\(filmInfo.date)"
         }
     }
     
--- Table View/FilmViewController.swift
+++ Table View/FilmViewController.swift
@@ -9,17 +9,44 @@ import UIKit
 
 class FilmViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    let cameraList = ["Leica Mini Zoom", "Leica Mini Zoom", "Leica Mini Zoom", "Leica Mini Zoom", "Konica Big Mini", "Fuji Simple Ace"]
-    let filmList = ["Fuji C200", "Fuji C200", "Kodak Gold 200", "Kodak Gold 200", "Agfa Vista 200", "embedded film"]
-    let filmLabList = ["Elly camera", "Elly camera", "Haedanghwa", "Mangwoosamlim", "Mangwoosamlim", "Mangwoosamlim"]
-    let dateList = ["October, 2020", "August, 2020", "January, 2020", "December, 2019", "August, 2019", "January, 2019"]
+    // MVVM
+    
+    // Model
+    // - FilmInfo
+    // > FilmInfo 만들자
+    
+    // View
+    // - ListCell
+    // > ListCell 에 필요한 정보를 ViewModel 에서 받아야한다.
+    // > ListCell 은 ViewModel 로 부터 받은 정보로 뷰 업데이트 하기
+    
+    // ViewModel
+    // - FilmViewModel
+    // > FilmViewModel 을 만들고, 뷰 레이어에서 필요한 메서드 만들기
+    // > 모델 가지고 있기 --> FilmInfo
+    
+    let filmInfoList: [FilmInfo] = [
+        FilmInfo(camera: "Leica Mini Zoom", film: "Fuji C200", filmLab: "Elly camera", date: "October, 2020"),
+        FilmInfo(camera: "Leica Mini Zoom", film: "Fuji C200", filmLab: "Elly camera", date: "August, 2020"),
+        FilmInfo(camera: "Leica Mini Zoom", film: "Kodak Gold 200", filmLab: "Haedanghwa", date: "January, 2020"),
+        FilmInfo(camera: "Leica Mini Zoom", film: "Kodak Gold 200", filmLab: "Mangwoosamlim", date: "December, 2019"),
+        FilmInfo(camera: "Konica Big Mini", film: "Agfa Vista 200", filmLab: "Mangwoosamlim", date: "August, 2019"),
+        FilmInfo(camera: "Fuji Simple Ace", film: "embedded film", filmLab: "Mangwoosamlim", date: "January, 2019")
+    ]
+    
+//    let cameraList = ["Leica Mini Zoom", "Leica Mini Zoom", "Leica Mini Zoom", "Leica Mini Zoom", "Konica Big Mini", "Fuji Simple Ace"]
+//    let filmList = ["Fuji C200", "Fuji C200", "Kodak Gold 200", "Kodak Gold 200", "Agfa Vista 200", "embedded film"]
+//    let filmLabList = ["Elly camera", "Elly camera", "Haedanghwa", "Mangwoosamlim", "Mangwoosamlim", "Mangwoosamlim"]
+//    let dateList = ["October, 2020", "August, 2020", "January, 2020", "December, 2019", "August, 2019", "January, 2019"]
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // DetailViewController 에게 데이터 넘겨준다.
         if segue.identifier == "showDetail" {
             let vc = segue.destination as? DetailViewController
             if let index = sender as? Int {
+                let filmInfo = filmInfoList[index]
                 vc?.index = index
+                vc?.filmInfo = filmInfo
             }
         }
     }
@@ -34,7 +61,8 @@ class FilmViewController: UIViewController, UITableViewDataSource, UITableViewDe
     // 따라서 Table view 와 view controller 에 ctrl + drag 한 후 delegate, datasource 선택해주어야 함!!
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cameraList.count
+//        return cameraList.count
+        return filmInfoList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -43,11 +71,18 @@ class FilmViewController: UIViewController, UITableViewDataSource, UITableViewDe
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
             return UITableViewCell()
         }
+//        let img = UIImage(named: "\(indexPath.row).jpg")
+//        cell.imgView.image = img
+//        cell.titleLabel.text = "\(cameraList[indexPath.row]) + \(filmList[indexPath.row]) developed by \(filmLabList[indexPath.row])"
+//        cell.nameLable.text = "Eunji Lee"
+//        cell.dateLabel.text = dateList[indexPath.row]
+        let filmInfo = filmInfoList[indexPath.row]
         let img = UIImage(named: "\(indexPath.row).jpg")
         cell.imgView.image = img
-        cell.titleLabel.text = "\(cameraList[indexPath.row]) + \(filmList[indexPath.row]) developed by \(filmLabList[indexPath.row])"
+        cell.titleLabel.text = "\(filmInfo.camera) + \(filmInfo.film) developed by \(filmInfo.filmLab)"
         cell.nameLable.text = "Eunji Lee"
-        cell.dateLabel.text = dateList[indexPath.row]
+        cell.dateLabel.text = filmInfo.date
+        
         return cell
     }
     
@@ -65,4 +100,21 @@ class ListCell: UITableViewCell {
     @IBOutlet weak var nameLable: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var imgView: UIImageView!
+}
+
+struct FilmInfo {
+    let camera: String
+    let film: String
+    let filmLab: String
+    let date: String
+    
+//    var image: UIImage? {
+//        return UIImage(named: "\(date).jpg")
+//    }
+    init(camera: String, film: String, filmLab: String, date: String) {
+        self.camera = camera
+        self.film = film
+        self.filmLab = filmLab
+        self.date = date
+    }
 }
